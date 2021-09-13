a36f2158bed21de6523c8895ba61ffc17584c591
Vadim2071 <vadim207138@gmail.com>
2021-09-13 15:43:13 +0300

    start independent work

---
+++ MetricsManager/DotNetMetric.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager
+{
+    public class DotNetMetric
+    {
+        public int ErrorCount { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
